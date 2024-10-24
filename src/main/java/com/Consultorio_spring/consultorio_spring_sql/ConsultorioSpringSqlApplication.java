4485983b9f0a5fb8164b1858c0d448e5f966d365
David_Morales <davidmorales.sena1823@gmail.com>
2024-10-24 15:35:53 -0500

    Consultorio Juridico Spring Boot

---
+++ src/main/java/com/Consultorio_spring/consultorio_spring_sql/ConsultorioSpringSqlApplication.java
@@ -0,0 +1,13 @@
+package com.Consultorio_spring.consultorio_spring_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioSpringSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioSpringSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Consultorio_spring/consultorio_spring_sql/ConsultorioSpringSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Consultorio_spring.consultorio_spring_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioSpringSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
